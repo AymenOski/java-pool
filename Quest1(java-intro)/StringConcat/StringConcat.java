2c1912b5a6d47cb058673954f77e75b6f32f61b7
AymenOski <aymenazizi610@gmail.com>
2026-04-05 16:32:08 +0100

    concatination with edge cases

--- Quest1(java-intro)/StringConcat/StringConcat.java
+++ Quest1(java-intro)/StringConcat/StringConcat.java
@@ -1,5 +1,8 @@
 public class StringConcat {
     public static String concat(String s1, String s2) {
+        if (s1 == null && s2 == null) {
+            return null;
+        }
         return (s1 == null ? "" : s1) + (s2 == null ? "" : s2);
     }
 }
