f18bf49f0c534c7001ed6991ad5e20ba26c8ec78
AymenOski <aymenazizi610@gmail.com>
2026-04-06 18:33:15 +0100

    cat

---
+++ Quest2(io-args)/Cat/Cat.java
@@ -0,0 +1,18 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class Cat {
+     public static void cat(String[] args) throws IOException {
+        if (args.length == 0) {
+            return;
+        }
+        try (FileInputStream file = new FileInputStream(args[0])) {
+            byte[] buffer = new byte[1024];
+            int bytesRead;
+
+            while ((bytesRead = file.read(buffer)) != -1) {
+                System.out.write(buffer, 0, bytesRead);
+            }
+        }
+    }
+}
