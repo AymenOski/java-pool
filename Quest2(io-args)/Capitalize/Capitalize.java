3ca51f4578427d588199c882bea6f16d5a247c0f
AymenOski <aymenazizi610@gmail.com>
2026-04-07 12:09:17 +0100

    changing the method

--- Quest2(io-args)/Capitalize/Capitalize.java
+++ Quest2(io-args)/Capitalize/Capitalize.java
@@ -1,34 +1,24 @@
 import java.io.*;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Capitalize {
-    public static void capitalize(String[] args) throws IOException {
-        if (args.length < 2) {
-            return;
-        }
-
-        String input = new String(Files.readAllBytes(Paths.get(args[0])));
-        StringBuilder result = new StringBuilder();
-
-        boolean newWord = true;
-
-        for (int i = 0; i < input.length(); i++) {
-            char c = input.charAt(i);
-
-            if (Character.isLetter(c)) {
-                if (newWord) {
-                    result.append(Character.toUpperCase(c));
-                    newWord = false;
-                } else {
-                    result.append(Character.toLowerCase(c));
+     public static void capitalize(String[] args) throws IOException {
+        if (args.length == 2) {
+            Path source = Paths.get(args[0]);
+            Path distination = Paths.get(args[1]);
+            String content = Files.readString(source);
+            String[] sl = content.split("\\s+");
+            List<String> newSl = new ArrayList<>();
+            for (String ele : sl) {
+                if (ele.length() > 0) {
+                    newSl.add(ele.substring(0, 1).toUpperCase() + ele.substring(1, ele.length()).toLowerCase());
                 }
-            } else {
-                result.append(c);
-                newWord = !Character.isDigit(c);
             }
+            Files.writeString(distination, String.join(" ", newSl).trim());
         }
-
-        Files.write(Paths.get(args[1]), result.toString().getBytes());
     }
 }
