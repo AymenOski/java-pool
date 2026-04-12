0537f96ecc567e4a6c6c3ceca9034ce3327745d5
AymenOski <aymenazizi610@gmail.com>
2026-04-12 13:16:39 +0100

    Gandalf with Frodon can beat Saroumane

---
+++ Quest4(oop2)/AdventureSorcerer/Sorcerer.java
@@ -0,0 +1,28 @@
+public class Sorcerer extends Character implements Healer{
+    private final int healCapacity;
+
+    public Sorcerer(String name, int maxHealth, int healCapacity){
+        super(name, maxHealth);
+        this.healCapacity = healCapacity;
+    }
+
+    // must implement 
+    public int getHealCapacity(){
+        return this.healCapacity;
+    }
+    public void heal(Character ch){
+        if (ch.getCurrentHealth() + this.healCapacity >= ch.getMaxHealth()){
+            ch.setCurrentHealth(ch.getMaxHealth());
+            return;
+        }
+        ch.setCurrentHealth(ch.getCurrentHealth() + this.healCapacity);
+    }
+
+    @Override
+    public String toString() {
+        if (this.getCurrentHealth() <= 0) {
+            return String.format("%s is a dead sorcerer. So bad, it could heal %d HP.", this.getName().substring(0, 1).toUpperCase() + this.getName().substring(1),this.getHealCapacity());
+        }
+        return String.format("%s is a sorcerer with %d HP. It can heal %d HP.", this.getName().substring(0, 1).toUpperCase() + this.getName().substring(1), this.getCurrentHealth(), this.getHealCapacity());
+    }
+}
