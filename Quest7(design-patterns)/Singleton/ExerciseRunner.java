7b7ac3f028dc658c3c749f9aff6ef596bc87f01d
AymenOski <aymenazizi610@gmail.com>
2026-04-23 12:58:54 +0100

    Singleton pattern

---
+++ Quest7(design-patterns)/Singleton/ExerciseRunner.java
@@ -0,0 +1,11 @@
+public class ExerciseRunner {
+
+    public static void main(String[] args)  {
+        System.out.println(Excalibur.getInstance().getName());
+    }
+}
+
+// $ javac *.java -d build
+// $ java -cp build ExerciseRunner 
+// Sword
+// $
